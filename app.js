590a73bf63d4f1a053b67b5f82b29ff20c509b4b
doron <doron.gruber@gmail.com>
2022-03-14 22:07:08 +0200

    update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const taskRouter = require('./back/routers/tasks');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist/todolistCalendar')));
+app.use(express.static(path.join(__dirname, 'dist','todolistCalendar')));
 
 app.use(cors());
 app.use(express.json({limit: '25mb'}));
@@ -30,7 +30,7 @@ app.use((req,res,next) => {
   });
 
 app.get('*', (req,res) => {
-  res.sendFile(path.join(__dirname, 'dist/todolistCalendar/index.html'))
+  res.sendFile(path.join(__dirname, 'dist','todolistCalendar','index.html'))
 })
 
 module.exports = app;
