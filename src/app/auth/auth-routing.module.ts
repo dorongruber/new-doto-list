6b4ffaee0b1236687349124126a117ba47127847
doron <doron.gruber@gmail.com>
2023-06-25 20:44:14 +0300

    fix responsive ui - auth model

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: '',
   component: AuthComponent, children: [
     {path: 'login', component: LoginComponent},
